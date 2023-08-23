167dbb1c508c12024b6bd9c3a84b1b2e1672b893
Test User <ahnaf.shakil24@gmail.com>
2023-08-23 17:43:04 +0600

    a

--- app.js
+++ app.js
@@ -4,5 +4,22 @@ const infoDisplay = document.querySelector("#info-display")
 const width = 8
 
 const startPieces =  [
-
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook,
 ]
+
+function createBoard() {
+    startPieces.forEach((startPiece) => {
+        const squre =document.createElement('div')
+        squre.classList.add('square')
+        squre.classList.add('beige')
+        gameBoard.append(squre)
+    })
+}
+ createBoard()
